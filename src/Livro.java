0af9610571164d9a478c3fbed5d76c9420b8cce6
kirigaya7447 <joaoh2915@gmail.com>
2026-08-15 20:28:59 -0300

    Organizando estrutura do projeto e arrumando a ordem dos gets e sets do Reserva

--- src/Livro.java
+++ src/Livro.java
@@ -13,44 +13,43 @@ public class Livro {
 
     }
 
-    public void setId_livro(int id_livro) {
-        this.id_livro = id_livro;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
     public int getId_livro() {
         return id_livro;
+    }
+
+    public void setId_livro(int id_livro) {
+        this.id_livro = id_livro;
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
     public String getAutor() {
         return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
     }
 
     public String getGenero() {
         return genero;
     }
 
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
     public String getStatus() {
         return status;
     }
-
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
